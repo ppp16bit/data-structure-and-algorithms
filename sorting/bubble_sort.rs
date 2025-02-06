bf1472277a033531f37ee9356e3baf7cec64e458
ppp16bit <pcampossobrinho@gmail.com>
2025-02-05 23:52:08 -0400

    bolhas

--- sorting/bubble_sort.rs
+++ sorting/bubble_sort.rs
@@ -1,10 +1,17 @@
-fn bubble_sort<T: Ord>(arr: &mut [T]) {
-    let n = arr.len();
+fn bubble_sort<T: Ord>(array: &mut [T]) {
+    let n = array.len();
+
     for i in 0..n {
         for j in 0..n - i - 1 {
-            if arr[j] > arr[j + 1] {
-                arr.swap(j, j + 1);
+            if array[j] > array[j + 1] {
+                array.swap(j, j + 1);
             }
         }
     }
+}
+
+fn main() {
+    let mut array = [64, 34, 25, 12, 22, 11, 90];
+    bubble_sort(&mut array);
+    println!("{:?}", array);
 }
