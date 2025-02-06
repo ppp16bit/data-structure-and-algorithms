412a1f05cc2110a28c817aa460a35f87a517135e
ppp16bit <pcampossobrinho@gmail.com>
2025-02-05 23:23:46 -0400

    first commit

---
+++ sorting/bubble_sort.rs
@@ -0,0 +1,10 @@
+fn bubble_sort<T: Ord>(arr: &mut [T]) {
+    let n = arr.len();
+    for i in 0..n {
+        for j in 0..n - i - 1 {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
