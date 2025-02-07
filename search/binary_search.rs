46ab57d067e1193200cecfb90b8e4e9e409f8c7f
ppp16bit <pcampossobrinho@gmail.com>
2025-02-07 00:17:08 -0400

    binary search

--- search/binary_search.rs
+++ search/binary_search.rs
@@ -14,3 +14,12 @@ fn binary_search(array: &[i32], target: i32) -> Option<usize> {
     }
     None
 }
+
+fn main() {
+    let array = vec![1, 3, 5, 7, 9, 11, 13];
+    let target = 7;
+
+    if let Some(index) = binary_search(&array, target) {
+        println!("{}", array[index]);
+    }
+}
