7a1fbaa9b0e616cb5a15c15ed44b5ea28a227088
ppp16bit <pcampossobrinho@gmail.com>
2025-02-17 23:38:38 -0400

    193ms les go

--- leetcode/5.py
+++ leetcode/5.py
@@ -1,21 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expand_around_center(left, right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return right - left - 1
+        if not s:
+            return ""
         
         start = 0
         end = 0
+        n = len(s)
         
-        for i in range(len(s)):
-            len1 = expand_around_center(i, i)
-            len2 = expand_around_center(i, i + 1)
-            
-            max_len = max(len1, len2)
-            
-            if max_len > end - start:
-                start = i - (max_len - 1) // 2
-                end = i + max_len // 2
+        
+        # no excessive func calls 
+        for i in range(n):
+            for left, right in [(i, i), (i, i + 1)]:
+                while left >= 0 and right < n and s[left] == s[right]:
+                    left -= 1
+                    right += 1
+                
+                if right - left - 1 > end - start:
+                    start, end = left + 1, right - 1
         return s[start:end + 1]
