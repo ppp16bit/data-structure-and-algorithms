05b1198f7b82e865a04e5427c3d31881268afda1
ppp16bit <pcampossobrinho@gmail.com>
2025-02-24 21:47:05 -0400

    453ms

--- leetcode/15.py
+++ leetcode/15.py
@@ -7,24 +7,27 @@ class Solution:
     nums.sort()
 
     for i in range(len(nums) - 2):
+      if nums[i] > 0:
+        break
+
       if i > 0 and nums[i] == nums[i - 1]:
         continue
   
-      l = i + 1
-      r = len(nums) - 1
+      l, r = i + 1, len(nums) - 1
       
       while l < r:
         summ = nums[i] + nums[l] + nums[r]
         if summ == 0:
-          ans.append((nums[i], nums[l], nums[r]))
+          ans.append([nums[i], nums[l], nums[r]])
           
           l += 1
-          r -= 1
-          
-          while nums[l] == nums[l - 1] and l < r:
+          while l < r and nums[l] == nums[l - 1]:
             l += 1
-          while nums[r] == nums[r + 1] and l < r:
+          
+          r -= 1
+          while l < r and nums[r] == nums[r + 1]:
             r -= 1
+
         elif summ < 0:
           l += 1
         else:
